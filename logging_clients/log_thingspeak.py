f81b3375545d9696d4f0de4c85b6bf86922f9dc4
Chris <sirleech@gmail.com>
2011-07-09 00:50:35 +1000

    first

---
+++ logging_clients/log_thingspeak.py
@@ -0,0 +1,31 @@
+import httplib, urllib
+import json
+from Modifiers import *
+
+# load your API key and Arduino IP address into the json file 'setings.js'
+# and specify the absolute path if running from Cron
+f = open('/root/dev/jsonDuino/logging_clients/creds.js', 'r')
+jsonObj = json.loads(f.read())
+apikey = jsonObj['thingspeak_apikey']
+arduino_ip = jsonObj['arduino_ip']
+arduino = httplib.HTTPConnection(arduino_ip)
+
+
+try:
+	arduino.request("GET","/a0")
+	response = arduino.getresponse()
+	print "Arduino response:" , response.status, response.reason
+	jsonObj = json.loads(response.read())
+	a0 = jsonObj['a0']
+	print "a0:", modify(a0)
+
+	params = urllib.urlencode({'field1': modify(a0),'key':apikey})
+	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	conn.request("POST", "/update", params, headers)
+	ts_response = conn.getresponse()
+	print "Thingspeak Response:", ts_response.status, ts_response.reason
+	conn.close
+except:
+	print "connection failed!"
+
---
+++ logging_clients/settings.js
@@ -0,0 +1,5 @@
+{
+	"arduino_ip" : "192.168.1.41",
+	"thingspeak_apikey" : "YOUR_KEY",
+	"pachube_apikey" : "YOUR_KEY"
+}
